93f25e247c850d2092099f771fa10d7f0eca6e0b
Rene Del Carpio <delcarpio.rene@gmail.com>
2022-07-06 21:19:08 -0400

    create variables and constructor

--- contracts/BikeRenting.sol
+++ contracts/BikeRenting.sol
@@ -2,10 +2,31 @@
 pragma solidity >=0.4.22 <0.9.0;
 
 contract BikeRenting {
+  address public owner;
+  mapping(address => uint) public balances;
   string public RoyalEnfield;
+  uint public RoyalEnfieldPrice;
   string public Triumph;
+  uint public TriumphPrice;
   string public BMW;
+  uint public BMWPrice;
   string public Aprilia;
+  uint public ApriliaPrice;
   string public Ducati;
+  uint public DucatiPrice;
+
+  constructor() {
+    owner = msg.sender;
+    RoyalEnfield = "Royal Enfield Meteor 350";
+    RoyalEnfieldPrice = 1200;
+    Triumph = "Triumph Trident 660";
+    TriumphPrice = 800;
+    BMW = "BMW R1250 GS";
+    BMWPrice = 1400;
+    Aprilia = "Aprilia RS 660";
+    ApriliaPrice = 1000;
+    Ducati = "Ducati Multistrada V4";
+    DucatiPrice = 1800;
+  }
 
 }
