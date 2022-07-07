478c202598b2b808f2788ec60430844f241d4997
Rene Del Carpio <delcarpio.rene@gmail.com>
2022-07-06 20:32:36 -0400

    create bikes renting project

---
+++ contracts/BikeRenting.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.4.22 <0.9.0;
+
+contract BikeRenting {
+  string public RoyalEnfield;
+  string public Triumph;
+  string public BMW;
+  string public Aprilia;
+  string public Ducati;
+
+}
